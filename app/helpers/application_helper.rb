1a3ce668c33b9367c6c6b1603130306040795684
pasluka <pasluka@gmail.com>
2016-03-07 17:36:33 +0100

    [TESA-215] inactive teachings mod update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,10 @@
 module ApplicationHelper
   def is_active_page(var, controller_action)
     if(!var.nil? && (controller_action == "weeks/enter_test" || controller_action == "weeks/list" ||
-        controller_action == "weeks/test_list" || controller_action == "weeks/index"))
+        controller_action == "weeks/test_list" || controller_action == "weeks/index" || controller_action == "teachings/show"))
       '-inactive'
     else
       ''
-    end
-  end
-
-  def is_active_page2(var)
-    if(var.nil?)
-      ''
-    else
-      '-inactive'
     end
   end
 end
